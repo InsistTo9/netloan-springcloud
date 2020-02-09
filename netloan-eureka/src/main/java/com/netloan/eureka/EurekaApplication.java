3f3839294f4e0d3eba5f1f57b2cc504b984f397d
InsistTo9 <lixh1520102@aliyun.com>
2020-02-09 12:11:49 +0800

    commit add eureka server

---
+++ netloan-eureka/src/main/java/com/netloan/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.netloan.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Administrator on 2020/1/31.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EurekaApplication.class, args);
+
+	}
+}
